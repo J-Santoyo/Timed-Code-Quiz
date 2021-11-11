17a4a8d6229ab54a5363c8c62ebf8316b3f1b7f0
J-Santoyo <joanasantoyo030@gmail.com>
2021-11-11 11:41:48 -0700

    added variables for questions three answer buttons

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 var timeLeft = 75;
-var quizTimerEl = document.getElementById("quiz-timer")
-var startBtn = document.getElementById("start-btn")
-var introEl = document.getElementById("intro")
-var questionsEl = document.getElementById("questions")
-var counter = 1
+var quizTimerEl = document.getElementById("quiz-timer");
+var startBtn = document.getElementById("start-btn");
+var introEl = document.getElementById("intro");
+var questionsEl = document.getElementById("questions");
+var counter = 1;
 
 // answer buttons for question one
-var answerA1 = document.getElementById("answer1a")
-var answerB1 = document.getElementById("answer1b")
-var answerC1 = document.getElementById("answer1c")
-var answerD1 = document.getElementById("answer1d")
+var answerA1 = document.getElementById("answer1a");
+var answerB1 = document.getElementById("answer1b");
+var answerC1 = document.getElementById("answer1c");
+var answerD1 = document.getElementById("answer1d");
 
 // answer buttons for question two
-var answerA2 = document.getElementById("answer2a")
-var answerB2 = document.getElementById("answer2b")
-var answerC2 = document.getElementById("answer2c")
-var answerD2 = document.getElementById("answer2d")
+var answerA2 = document.getElementById("answer2a");
+var answerB2 = document.getElementById("answer2b");
+var answerC2 = document.getElementById("answer2c");
+var answerD2 = document.getElementById("answer2d");
 
-// answer 
+// answer buttons for question three
+var answerA3 = document.getElementById("answer3a");
+var answerB3 = document.getElementById("answer3b");
+var answerC3 = document.getElementById("answer3c");
+var answerD3 = document.getElementById("answer3d");
+
+
 
 // This function starts the timer.
 function startTimer() {
